94070ca004ba171be2fa5c4398d6f42f170183fb
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-03-24 23:08:46 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -14,12 +14,12 @@ class App < Sinatra::Base
     end
 
     post '/checkout' do
-       session[:item] = "item"
-       @session = session
-        # @item = params[:item]
-        # session["item"] = @item
+      # session[:item] = "item"
+      # @session = session
+        @item = params[:item]
+        session["item"] = @item
         
-        # erb :'checkout'
+        erb :'checkout'
     end
 end
 
