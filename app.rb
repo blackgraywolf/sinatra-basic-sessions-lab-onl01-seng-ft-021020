3572f645a76c8ca99cad5191c9c289610a5602bf
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-03-24 23:00:31 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,20 +1,24 @@
+
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  configure do
-    enable :sessions
-    set :session_secret, "secret"
-  end
 
-  get '/' do
-    erb :index
-  end
+    configure do
+        enable :sessions
+        set :session_secret, "secret"
+    end
 
-  post '/checkout' do
-    @sessions = session
-    item = params["item"]
-    @sessions[:item] = item
-  end
+    get '/' do
+        @session = session
+        erb :'index'
+    end
+
+    post '/checkout' do
+        @item = params[:item]
+        session["item"] = @item
+        
+        erb :'checkout'
+    end
 end
 
 # 	 get '/hey' do 
